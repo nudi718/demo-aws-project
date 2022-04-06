f205042d376cdaf2973ee0bbf57819883f3bc984
nudi718 <68341958+nudi718@users.noreply.github.com>
2022-04-06 15:17:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "pro2db.c2ixcvdpcfnx.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "admin123"
-customdb = "PRO2db"
+customdb = "pro2db"
 custombucket = "proj2bu"
 customregion = "us-east-1"
